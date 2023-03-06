40203d44f440d661aa15bc5d34e839201c2c9c26
Milos Vasic <i@mvasic.ru>
2023-03-06 15:33:17 +0100

    Recipe: Init. Software Toolkit.

--- Services-Recipes/CPP_Drogon/recipe_api.sh
+++ Services-Recipes/CPP_Drogon/recipe_api.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SERVICE_NAME_SUFFIX="-API"
+SCRIPT_TOOLKIT_INIT="initialize.sh"
+
 if [ -z "$1" ]; then
 
     echo "ERROR: Path is mandatory parameter for the API generating"
@@ -16,9 +19,27 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
-SERVICE_NAME="$2-API"
+SERVICE_NAME="$2$SERVICE_NAME_SUFFIX"
 
 echo "CPP Drogon recipe, generate API to '$PATH'"
+
+if ! test -e "$SCRIPT_TOOLKIT_INIT"; then
+
+    echo "ERROR: Software Toolkit initialization script not found at '$SCRIPT_TOOLKIT_INIT'"
+    exit 1
+fi
+
+echo "Initializing Software Toolkit"
+
+if sh "$SCRIPT_TOOLKIT_INIT" "$PATH" "$SERVICE_NAME"; then
+
+    echo "Software Toolkit initialized"
+
+else
+
+    echo "ERROR: Software Toolkit not initialized"
+    exit 1
+fi
 
 # TODO
 
