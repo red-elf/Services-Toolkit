0d3808123bb075197381fb27ddd5d6d4ec8d2183
Milos Vasic <i@mvasic.ru>
2023-03-06 17:00:51 +0100

    FIXME added.

--- Services-Recipes/CPP_Drogon/recipe_api.sh
+++ Services-Recipes/CPP_Drogon/recipe_api.sh
@@ -34,6 +34,11 @@ if test -e "$SCRIPT_TOOLKIT_INIT"; then
 
     echo "Initializing Software Toolkit"
 
+    # FIXME:
+    # Initializing Software Toolkit
+    # Services-Recipes/CPP_Drogon/recipe_api.sh: line 37: sh: command not found
+    # ERROR: Software Toolkit not initialized
+    # ERROR: Recipe failed 'Services-Recipes/CPP_Drogon/recipe_api.sh'
     if sh $SCRIPT_TOOLKIT_INIT $PATH $SERVICE_NAME; then
 
         echo "Software Toolkit initialized"
